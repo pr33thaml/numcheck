a18805128a91351873253adef76fa17d1541db2d
pr33thaml <preethaml99@outlook.com>
2025-06-07 22:01:48 +0530

    app.py updates

--- app.py
+++ app.py
@@ -223,4 +223,4 @@ def clear_history():
     return jsonify({'message': 'Search history cleared'})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000) 
+    app.run(host='0.0.0.0', port=5000)
